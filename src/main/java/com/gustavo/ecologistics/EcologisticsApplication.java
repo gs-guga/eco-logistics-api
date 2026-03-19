d70d22a255d38caea7ac4351e71b32036233b0a8
Gustavo <gustavocarvalho933085@gmail.com>
2026-03-19 12:17:37 -0300

    first commit

---
+++ src/main/java/com/gustavo/ecologistics/EcologisticsApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.ecologistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcologisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcologisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/ecologistics/EcologisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.ecologistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcologisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
